24ace32c4b1874071816335fc41e3537494931c4
Ciro Moniz <cirinho@canalbahia.net>
2024-03-14 03:50:23 -0300

    Transforma em Addon Mk-Auth #1

---
+++ recibo/index.php
@@ -0,0 +1,27 @@
+<?php
+// INCLUE FUNCOES DE ADDONS -----------------------------------------------------------------------
+include('addons.class.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-BR" class="has-navbar-fixed-top">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="iso-8859-1">
+<title>MK - AUTH :: <?php echo $Manifest->{'name'}; ?></title>
+
+<link href="../../estilos/mk-auth.css" rel="stylesheet" type="text/css" />
+<link href="../../estilos/font-awesome.css" rel="stylesheet" type="text/css" />
+
+<script src="../../scripts/jquery.js"></script>
+<script src="../../scripts/mk-auth.js"></script>
+
+</head>
+<body>
+
+<?php include('../../topo.php'); ?>
+<?php include('recibo.php'); ?>
+<?php include('../../baixo.php'); ?>
+
+<script src="../../menu.js.hhvm"></script>
+
+</body>
